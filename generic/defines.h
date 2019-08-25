9262a7560d7df10a5a05a68d0b672e06bf8cc9e9
npasson <passon@campus.tu-berlin.de>
2019-08-25 10:00:29 +0200

    Added builtin (un)likely

--- generic/defines.h
+++ generic/defines.h
@@ -7,6 +7,9 @@
 
 #include <cstdint>
 
+#define LIKELY(x)       __builtin_expect(static_cast<bool>(x),1)
+#define UNLIKELY(x)     __builtin_expect(static_cast<bool>(x),0)
+
 typedef uint16_t imageDimension_t;
 typedef uint8_t  byte_t;
 
